e53c5f66b0fa9d5a20fbd4e91309a119b52f234a
Max Rogério <max10rogerio@hotmail.com>
2022-10-19 16:12:27 -0300

    incomplete example

---
+++ README.md
@@ -0,0 +1,6 @@
+# Docker with HTTPS
+
+Structure:
+- nginx
+- nestjs (api)
+- nextjs (web)
---
+++ frontend/web/pages/test.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Test: NextPage = () => {
+  return <h1>dsaljdsa</h1>;
+};
+
+export default Test;
